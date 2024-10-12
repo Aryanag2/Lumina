baa9fe4038ff5b8c1f4a85d09f66c9d371b2fd90
Nittin Murthi <ndhek@users.noreply.huggingface.co>
2024-10-12 02:55:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ from gradio_demo import build_app
 
 if __name__ == "__main__":
     app = build_app()
-    app.queue().launch(server_name="0.0.0.0", server_port=4700, share=True)
+    #app.queue().launch(server_name="0.0.0.0", server_port=4700, share=True)
+    app.launch(share=True)
